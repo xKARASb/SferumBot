5c654ade0ef80b4331ee614c32ec5cdf780b2e97
Maxim <maxkolosda@yandex.ru>
2023-04-07 19:07:25 +0300

    init commit + raw chat memmbers parsing

--- web_window.py
+++ web_window.py
@@ -5,12 +5,13 @@ from subprocess import CREATE_NO_WINDOW
 
 
 import time, json, ast, requests
-import os, websockets, asyncio
+import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
 auth_cookie = os.getenv("COOKIE")
+bot_token = os.getenv("BOT_TOKEN")
 
 caps = DesiredCapabilities.CHROME
 caps['goog:loggingPrefs'] = {'performance': 'ALL'}
@@ -18,7 +19,7 @@ options = webdriver.ChromeOptions()
 
 options.add_argument('--log-level=3')
 options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36 OPR/97.0.0.0")
-options.add_argument("--headless")
+#options.add_argument("--headless")
 options.add_argument('remote-debugging-port=9222')
 
 chrome_service = ChromeService()
@@ -27,39 +28,16 @@ chrome_service.creationflags = CREATE_NO_WINDOW
 driver = webdriver.Chrome(executable_path="D:\\works\\coding\\python\\sferum\\driver\\chromedriver.exe", options=options, desired_capabilities=caps, service=chrome_service)
 
 
-print(driver.capabilities) 
-
-
 def send_msg(text):
   params = {
      "chat_id": "-1001917922644",
      "text": text,
   }
-  requests.get("https://api.telegram.org/bot6203980861:AAGs41AffVm4Kib88x5m85kqBnVz8OJCcxI/sendMessage", params=params)
-
-#async with websockets.connect("") as ws:
-#  pass
+  requests.get("https://api.telegram.org/bot{bot_token}/sendMessage", params=params)
 
 def process_browser_log_entry(entry):
     response = json.loads(entry['message'])['message']
     return response
-
-#def logging(entity: list):
-#    for el in entity:
-#        print(el)
-#        print()
-#        print(type(el))
-#        if el["url"] == "https://api.vk.me/ruim791593813?version=19&mode=682":
-#            pass
-          #print(type(el))
-
-          #print(el)
-          #print()
-#          resp = driver.execute_cdp_cmd('Network.responseReceived', {'requestId': el["params"]["requestId"]})
-#          write = f"{el}\n{resp}"
-#          with open("log.json", "w") as f:
-#              json.dump(write, f)
-          
 
 
 driver.get("https://web.vk.me")
@@ -68,15 +46,26 @@ driver.get("https://web.vk.me")
 driver.add_cookie({"name": "remixdsid", "value": auth_cookie, "domain":"web.vk.me", "path":"/", "secure": True})
 driver.refresh()
 
+time.sleep(5)
+
 browser_log = driver.get_log('performance')
-#events = [process_browser_log_entry(entry) for entry in browser_log]
 
-#for el in events:
-#  if el["method"] == "Network.Request":
-#    if el['params']["response"]["url"] == "https://api.vk.me/method/account.setOnline?v=5.204":
-#        print(el)
-#        
+events = [process_browser_log_entry(entry) for entry in browser_log]
 
+for el in events:
+  if el["method"] == "Network.requestWillBeSent":
+      if el["params"]["request"]["url"] == "https://api.vk.me/method/account.setOnline?v=5.204":
+        auth_token = el["params"]["request"]["postData"].split("=")[1]
+
+        data= {"peer_id": 2000000001, "start_cmid": -1, "count": 10, "offset":0, "extended": 1, "group_id": 0, "fields": "id,first_name,first_name_gen,first_name_acc,last_name,last_name_gen,last_name_acc,sex,has_photo,photo_id,photo_50,photo_100,photo_200,contact_name,occupation,bdate,city,screen_name,online_info,verified,blacklisted,blacklisted_by_me,can_call,can_write_private_message,can_send_friend_request,can_invite_to_chats,friend_status,followers_count,profile_type,contacts,name,type,members_count,member_status,is_closed,can_message,deactivated,activity,ban_info,is_messages_blocked,can_post_donut,site", "access_token":auth_token}
+        resp = requests.post("https://api.vk.me/method/messages.getConversationMembers?v=5.204", data=data)
+
+        for i in resp.json()["response"]["profiles"]:
+          print(i)
+
+        print()
+        
+print("while")
 
 while True:
   time.sleep(10)
