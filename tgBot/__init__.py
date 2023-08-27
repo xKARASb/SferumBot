3a0773e2bafa61ebe4d5d9e4df7eff6d52e665a7
Maxim <maxkolosda@yandex.ru>
2023-08-27 23:15:02 +0300

    fix vote user filter

--- tgBot/__init__.py
+++ tgBot/__init__.py
@@ -7,7 +7,7 @@ from aiogram import Dispatcher, F
 from vk.methods import send_message, send_vote
 
 load_dotenv()
-host_id = getenv("TG_USER_ID")
+host_id = int(getenv("TG_USER_ID"))
 
 dp = Dispatcher()
 
@@ -16,10 +16,11 @@ async def on_message(message: Message) -> None:
     from main import bot
     send_message(bot.access_token, bot.peer_id, message.text)
 
-@dp.poll_answer(F.from_user.id==host_id)
+@dp.poll_answer()
 async def on_poll_answer(poll_answer: PollAnswer) -> None:
-    from main import bot
-    send_vote(poll_answer, bot.access_token)
+    if poll_answer.user.id == host_id:
+        from main import bot
+        send_vote(poll_answer, bot.access_token)
 
 async def start_polling(bot) -> None:
     await dp.start_polling(bot)
