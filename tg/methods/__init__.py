679389d391a2cb009f845bdbbd19f67096f8ee83
Максим Колос <maxkolosda@yandex.ru>
2025-03-26 15:12:37 +0300

    Add send_error method

--- tg/methods/__init__.py
+++ tg/methods/__init__.py
@@ -1,5 +1,6 @@
 """Just init file."""
 
+from .send_error import send_error
 from .send_message import send_message
 
-__all__ = [send_message]
+__all__ = [send_message, send_error]
---
+++ tg/methods/send_error.py
@@ -0,0 +1,15 @@
+"""Send msg to telegram."""
+
+from aiogram import Bot
+
+async def send_error(
+    bot: Bot,
+    tg_chat_id: int,
+    tg_topic_id: int | None = None,
+) -> None:
+    """Send error notify to telegram."""
+    await bot.send_message(
+        text="Сообщение из сферума не смогло обработаться\.\.\.",
+        chat_id=tg_chat_id,
+        message_thread_id=tg_topic_id,
+    )
